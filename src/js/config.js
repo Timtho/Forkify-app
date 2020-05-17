2f0c4852e8ecd185db8a2ef99bb78e19565719c1
Timtho <maciej.karasinski.95@gmail.com>
2020-05-17 11:22:59 +0200

    API changed

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,2 @@
-
-export const key = "5422923071199d0291924635eb7728f8";
-export const proxy = "https://cors-anywhere.herokuapp.com/";
+//export const key = "5422923071199d0291924635eb7728f8";
+//export const proxy = "https://cors-anywhere.herokuapp.com/";
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,6 +1,5 @@
-import axios from "axios";
+import axios from 'axios';
 import { key, proxy } from '../config';
-
 
 export default class Search {
   constructor(query) {
@@ -10,8 +9,10 @@ export default class Search {
   async getResults() {
     try {
       //${proxy}
-      const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-      //const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+      //const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      const res = await axios(
+        `https://forkify-api.herokuapp.com/api/search?&q=${this.query}`
+      );
 
       this.result = res.data.recipes;
     } catch (error) {
